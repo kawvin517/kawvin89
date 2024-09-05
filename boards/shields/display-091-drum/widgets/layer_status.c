38392e8dbf80722cb5e82354476978972e6dcca2
kawvin517 <kawvin@sina.com>
2024-09-05 18:07:04 +0800

    layery文字标识3-5度

--- boards/shields/display-091-drum/widgets/layer_status.c
+++ boards/shields/display-091-drum/widgets/layer_status.c
@@ -102,7 +102,7 @@ static void set_layer_symbol(lv_obj_t *label, struct layer_status_state state) {
     }
     // lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
     // lv_obj_center(label);
-    // lv_obj_set_style_transform_angle(label, 900, 0);
+    lv_obj_set_style_transform_angle(label, 50, 0);
     
  
 }
